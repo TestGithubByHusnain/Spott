c0dae34e71add3ffff94f88e8748aae5a57525e1
Husnain Ali <husnain.dev.009@gmail.com>
2025-12-08 11:54:00 +0500

    Implementing clerk and convex

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,18 @@
+"use client";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { BarLoader } from "react-spinners";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { useStoreUser } from "@/hooks/use-store-user";
+import { Building, Plus, Ticket } from "lucide-react";
 
-const Header = () => {
+export default function Header() {
+  const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+  const { isLoading } = useStoreUser();
+
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -17,10 +27,62 @@ const Header = () => {
               priority
             />
           </Link>
+
+          <div className="flex items-center">
+            <Button variant={"ghost"} size="sm" onClick={() =>setShowUpgradeModal(true)}>
+              Pricing
+            </Button>
+
+            <Button variant={"ghost"} size="sm" className={"mr-2"}>
+              <Link href="explore">Explore</Link>
+            </Button>
+
+            <Authenticated>
+              <Button size="sm" asChild className="flex gap-2 mr-4">
+                <Link href="/create-event">
+                  <Plus className="w-4 h-4" />
+                  <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+              {/* User Button */}
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    avatarBox: "w-9 h-9",
+                  },
+                }}
+              >
+                <UserButton.MenuItems>
+                  <UserButton.Link
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-tickets"
+                  />
+                  <UserButton.Link
+                    label="My Events"
+                    labelIcon={<Building size={16} />}
+                    href="/my-events"
+                  />
+                  <UserButton.Action label="manageAccount" />
+                </UserButton.MenuItems>
+              </UserButton>
+            </Authenticated>
+            <Unauthenticated>
+              <SignInButton mode="modal">
+                <Button size="sm">Sign In</Button>
+              </SignInButton>
+            </Unauthenticated>
+          </div>
         </div>
+
+        {/* Loader */}
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#8a55f7" />
+          </div>
+        )}
       </nav>
     </>
   );
-};
-
-export default Header;
+}
